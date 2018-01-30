d113e4667d5894ae404dbe40d938535e3ad3e966
Villermen <villermen@gmail.com>
2018-01-31 00:02:22 +0100

    Move activity comparison equality check behind missing activity fix

--- src/HighScore/HighScoreComparisonActivity.php
+++ src/HighScore/HighScoreComparisonActivity.php
@@ -23,16 +23,16 @@ class HighScoreComparisonActivity extends HighScoreComparisonEntry
     {
         parent::__construct($activity1, $activity2);
 
-        if ($activity1->getActivity() !== $activity2->getActivity()) {
-            throw new RuneScapeException("Can't compare two different activities.");
-        }
-
         if (!$activity1) {
             $activity1 = new HighScoreActivity($activity2->getActivity(), -1, -1);
         }
 
         if (!$activity2) {
             $activity2 = new HighScoreActivity($activity1->getActivity(), -1, -1);
+        }
+
+        if ($activity1->getActivity() !== $activity2->getActivity()) {
+            throw new RuneScapeException("Can't compare two different activities.");
         }
 
         $this->activity1 = $activity1;
